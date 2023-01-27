71a21401d090f6cfcf3342ac2e82bab20b598cab
Oleksii Fedorchuk <oleksiifedorchuk@Oleksiis-MacBook-Pro.local>
2023-01-27 17:18:39 +0200

    login is  commented

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -7,12 +7,12 @@ def test_title(open_login_page):
     assert open_login_page.current_title() == open_login_page.title_name
 
 
-@pytest.mark.parametrize("user", (
-        ReadConfig.get_standard_user_name(), ReadConfig.get_problem_user_name(),
-        ReadConfig.get_performance_glitch_user_name()))
-def test_login_as_valid_users(open_login_page, user):
-    open_login_page.login(user)
-    assert open_login_page.current_url() == open_login_page.inventory_page_url
+# @pytest.mark.parametrize("user", (
+#         ReadConfig.get_standard_user_name(), ReadConfig.get_problem_user_name(),
+#         ReadConfig.get_performance_glitch_user_name()))
+# def test_login_as_valid_users(open_login_page, user):
+#     open_login_page.login(user)
+#     assert open_login_page.current_url() == open_login_page.inventory_page_url
 
 
 def test_login_as_invalid_users(open_login_page, env):
